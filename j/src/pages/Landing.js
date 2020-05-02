0f334c1199584b802f1a83d261024327e7fbbc5b
Jorge Fuentes <jsjfuentesj@gmail.com>
2020-05-02 15:46:06 -0700

    Basic editor

--- j/src/pages/Landing.js
+++ j/src/pages/Landing.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
 import { Link } from "react-router-dom";
 
 import { axios } from "utils.js";
@@ -6,18 +8,13 @@ import Navbar from "components/Navbar";
 import Footer from "components/Footer";
 
 export default function Landing() {
-  const [resp, setResp] = useState("");
-  useEffect(() => {
-    axios
-      .get("/test")
-      .then((response) => {
-        console.log("Haha", response);
-        setResp(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }],
+    },
+  ]);
 
   return (
     <>
@@ -27,22 +24,14 @@ export default function Landing() {
         </Link>
       </Navbar>
 
-      <div className="w-full p-8">
-        <div className="px-4 pb-4 flex flex-col justify-center items-center">
-          <div className="text-4xl block font-bold mb-4">
-            Welcome to React Base
-          </div>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-4"
-            type="button"
-          >
-            Useless Button
-          </button>
-          <div className="text-2xl block font-bold mb-4">
-            {resp ? `The Server Says "${resp}"` : "The Server Can't Be Reached"}
-          </div>
-        </div>
-      </div>
+      <Slate
+        editor={editor}
+        value={value}
+        onChange={(value) => setValue(value)}
+      >
+        <Editable />
+      </Slate>
+
       <Footer />
     </>
   );
