73f12845b3b056e9e91cc7b7470609f83fe1ecb9
Jorge Fuentes <jsjfuentesj@gmail.com>
2020-05-02 20:32:12 -0700

    Add toolbar and bolding

--- j/src/pages/Landing.js
+++ j/src/pages/Landing.js
@@ -1,11 +1,50 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { createEditor } from "slate";
+import React, { useEffect, useMemo, useState, useCallback } from "react";
+import { createEditor, Transforms, Editor, Text } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 import { Link } from "react-router-dom";
 
 import { axios } from "utils.js";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
+const debug = require("debug")("app:Landing");
+
+// Define our own custom set of helpers.
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.bold === true,
+      universal: true,
+    });
+
+    return !!match;
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.type === "code",
+    });
+
+    return !!match;
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor);
+    Transforms.setNodes(
+      editor,
+      { bold: isActive ? null : true },
+      { match: (n) => Text.isText(n), split: true }
+    );
+  },
+
+  toggleCodeBlock(editor) {
+    const isActive = CustomEditor.isCodeBlockActive(editor);
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : "code" },
+      { match: (n) => Editor.isBlock(editor, n) }
+    );
+  },
+};
 
 export default function Landing() {
   const editor = useMemo(() => withReact(createEditor()), []);
@@ -16,6 +55,19 @@ export default function Landing() {
     },
   ]);
 
+  const renderElement = useCallback((props) => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
+  const renderLeaf = useCallback((props) => {
+    return <Leaf {...props} />;
+  }, []);
+
   return (
     <>
       <Navbar>
@@ -23,16 +75,78 @@ export default function Landing() {
           Login
         </Link>
       </Navbar>
-
       <Slate
         editor={editor}
         value={value}
         onChange={(value) => setValue(value)}
       >
-        <Editable />
-      </Slate>
+        <div>
+          <button
+            onMouseDown={(event) => {
+              event.preventDefault();
+              CustomEditor.toggleBoldMark(editor);
+            }}
+          >
+            Bold
+          </button>
+          <button
+            onMouseDown={(event) => {
+              event.preventDefault();
+              CustomEditor.toggleCodeBlock(editor);
+            }}
+          >
+            Code Block
+          </button>
+        </div>
+        <Editable
+          editor={editor}
+          renderElement={renderElement}
+          renderLeaf={renderLeaf}
+          onKeyDown={(event) => {
+            if (!event.ctrlKey) {
+              return;
+            }
 
+            switch (event.key) {
+              case "`": {
+                event.preventDefault();
+                CustomEditor.toggleCodeBlock(editor);
+                break;
+              }
+
+              case "b": {
+                event.preventDefault();
+                CustomEditor.toggleBoldMark(editor);
+                break;
+              }
+            }
+          }}
+        />
+      </Slate>
       <Footer />
     </>
   );
 }
+
+const CodeElement = (props) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+};
+
+const DefaultElement = (props) => {
+  return <p {...props.attributes}>{props.children}</p>;
+};
+
+const Leaf = (props) => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? "bold" : "normal" }}
+    >
+      {props.children}
+    </span>
+  );
+};
